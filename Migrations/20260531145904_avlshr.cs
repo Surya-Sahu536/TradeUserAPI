bf4e67b896e967be41eaff80702fa838c3167bd7
surya <suryasahu114@gmail.com>
2026-06-01 00:48:41 +0530

    signalr

---
+++ Migrations/20260531145904_avlshr.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TradeUserAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class avlshr : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "AvailableShares",
+                table: "Stocks",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "TotalShares",
+                table: "Stocks",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvailableShares",
+                table: "Stocks");
+
+            migrationBuilder.DropColumn(
+                name: "TotalShares",
+                table: "Stocks");
+        }
+    }
+}
